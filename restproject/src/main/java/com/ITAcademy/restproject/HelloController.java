0571fc8de268a36e337ffe3bc8a527638d593212
Anton Lage <hagane.link0gmail.com>
2020-05-29 13:00:07 +0200

    Added Phase 02

--- restproject/src/main/java/com/ITAcademy/restproject/HelloController.java
+++ restproject/src/main/java/com/ITAcademy/restproject/HelloController.java
@@ -2,19 +2,25 @@ package com.ITAcademy.restproject;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
   private static final String template = "Hello, %s!";
 
+//  @GetMapping("/hello-world")
+//  @ResponseBody
+//  public Greeting sayHello(@RequestParam(name="name") String name) {
+//    return new Greeting(String.format(template, name));
+//  }
+  
   @GetMapping("/hello-world")
-  @ResponseBody
-  public Greeting sayHello(@RequestParam(name="name") String name) {
-    return new Greeting(String.format(template, name));
+  public String sayHello(@RequestParam(name="name") String name, Model model) {
+    model.addAttribute( "greeting", new Greeting(String.format(template, name)));
+    return "hello";
   }
 
 }
