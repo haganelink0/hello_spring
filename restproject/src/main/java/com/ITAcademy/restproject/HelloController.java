6647375277fa4a21a771396f1a3ac0fc30f36268
Anton Lage <hagane.link0gmail.com>
2020-07-06 13:47:01 +0200

    Fixed error for test

--- restproject/src/main/java/com/ITAcademy/restproject/HelloController.java
+++ restproject/src/main/java/com/ITAcademy/restproject/HelloController.java
@@ -5,13 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
   private static final String template = "Hello, %s!";
 
-//  @GetMapping("/hello-world")
+//  @GetMapping("/")
 //  @ResponseBody
 //  public Greeting sayHello(@RequestParam(name="name") String name) {
 //    return new Greeting(String.format(template, name));
@@ -20,7 +21,7 @@ public class HelloController {
   @GetMapping("/hello-world")
   public String sayHello(@RequestParam(name="name") String name, Model model) {
     model.addAttribute( "greeting", new Greeting(String.format(template, name)));
-    return "hello";
+    return "hello.html";
   }
 
 }
